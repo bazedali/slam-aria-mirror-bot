37c39cf6d804177a7b12a76c1abe9ff1eee72119
Mohammed Bazed Ali <mbazedali@gmail.com>
2022-05-12 19:31:49 +0530

    Update bot_utils.py
    GDTot

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -222,6 +222,10 @@ def is_url(url: str):
     url = re.findall(URL_REGEX, url)
     return bool(url)
 
+def is_gdtot_link(url: str):
+    url = match(r'https?://.+\.gdtot\.\S+', url)
+    return bool(url)
+
 def is_gdrive_link(url: str):
     return "drive.google.com" in url
 
